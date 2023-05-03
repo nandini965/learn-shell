91474cca6eae0a977617339fff2618a5f2938a41
nandini965 <nandhini965@gmail.com>
2023-05-03 16:07:58 +0530

    special variables for inputs

--- 03 variable.sh
+++ 03 variable.sh
@@ -7,3 +7,9 @@ DATE=$(date +%F)
 echo today date is ${DATE}
 ARTH=$((2-3*4/2))
 echo ARTH = ${ARTH}
+## special variables for inputs
+echo script name -$0
+echo first arguement -$1
+echo second arguement -$2
+echo all arguments -$*
+echo n0 of arguements -$#
