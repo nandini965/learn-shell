c4aa9d870b0b943f549118376b58b216e2aa1de6
nandini965 <nandhini965@gmail.com>
2023-05-11 09:34:52 +0530

    05-if.sh

--- 05-if.sh
+++ 05-if.sh
@@ -3,5 +3,10 @@ quantity=10
 if [ $fruit_name == "mango" ]
 then
 echo mango quantity - $quantity
-
 fi
+
+fruit_name=$1
+quantity=$2
+if [ "fruit_name" == "mango" ]
+then
+  echo mango quantity - $quantity
